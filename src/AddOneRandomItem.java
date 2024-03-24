fd3a8f85aa7ca35a47e2c75e925de5567d45e5d7
nagham116 <naghamaqeel11@gmail.com>
2024-03-24 23:33:53 +0300

    update

--- src/AddOneRandomItem.java
+++ src/AddOneRandomItem.java
@@ -1,10 +1,14 @@
+import static org.testng.AssertJUnit.assertEquals;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
+import dev.failsafe.internal.util.Assert;
 
-	import java.util.List;
+import java.time.Duration;
+import java.util.List;
 	import java.util.Random;
 
 	import org.openqa.selenium.By;
@@ -20,16 +24,17 @@ import org.testng.annotations.Test;
 		WebDriver driver = new ChromeDriver();
 		String URL = "https://magento.softwaretestingboard.com/";
 
-		
+		String Password = "ASDasd123!@";
 
 		@BeforeTest
 
 		public void mySetup() {
 			driver.manage().window().maximize();
+			driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(4));
 		}
 		
 
-		@Test(invocationCount = 1, priority = 1, description = "this is my 1st test")
+		@Test
 		public void addOneRandomItemToThecart() throws InterruptedException {
 			Random rand = new Random();
 			driver.get(URL);
@@ -66,6 +71,72 @@ import org.testng.annotations.Test;
 			}
 			 
 			}
-	
+	      @Test
+		public void CheckoutProcess() {
+			
+		
+		}
+
+		@Test
+		public void checkoutProcess() throws InterruptedException {
+			String CheckoutPage = "https://magento.softwaretestingboard.com/checkout/cart/";
+			driver.get(CheckoutPage);
+			Thread.sleep(3000);
+			WebElement proceedButton = driver.findElement(By.xpath("//button[@data-role='proceed-to-checkout']"));
+			proceedButton.click();
 
 		}
+
+		@Test
+		public void SignupProcess() throws InterruptedException {
+			
+			Thread.sleep(4000);
+			WebElement email = driver.findElement(By.id("customer-email"));
+			WebElement firstName = driver.findElement(By.name("firstname"));
+			WebElement lastName = driver.findElement(By.name("lastname"));
+			WebElement StreetAddress = driver.findElement(By.name("street[0]"));
+			WebElement city = driver.findElement(By.name("city"));
+			WebElement state = driver.findElement(By.name("region_id"));
+			WebElement postalCode = driver.findElement(By.name("postcode"));
+			WebElement Country = driver.findElement(By.name("country_id"));
+			WebElement phoneNumber = driver.findElement(By.name("telephone"));
+			WebElement nextButton = driver.findElement(By.cssSelector(".button.action.continue.primary"));
+
+			email.sendKeys("asaqa0133@gmail.com");
+			firstName.sendKeys("abed");
+			lastName.sendKeys("alraheem");
+	 
+			StreetAddress.sendKeys("amman tlaa alali");
+
+			city.sendKeys("amman");
+
+			state.sendKeys("tlaa alali");
+
+			postalCode.sendKeys("19933");
+
+			Country.sendKeys("Jordan");
+
+			phoneNumber.sendKeys("962797700235");
+
+			nextButton.click();
+
+			Select select = new Select(Country);
+
+//			select.selectByValue("CN");
+//			select.selectByIndex(1);
+			select.selectByVisibleText("Jordan");
+			
+		}
+	}
+	    	  
+	    	  
+	    	  
+	    	  
+	    	  
+	    	  
+	    	  
+	    	  
+	      
+	      
+
+		
